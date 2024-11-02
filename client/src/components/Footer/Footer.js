df211b284613491bbafed9ed4ab294dcff3c6812
fridahnamudu <fridah.namudu@gmail.com>
2024-11-02 20:42:56 +0300

    add footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,71 +1,46 @@
-// Footer.js
-import React from 'react';
-import { Box, Typography, Grid, Link, IconButton } from '@mui/material';
-import { Facebook, Twitter, Instagram, LinkedIn } from '@mui/icons-material';
-import './Footer.css';
+import React from 'react'
+import './Footer.css'
+import PhoneForwardedIcon from '@mui/icons-material/PhoneForwarded';
+import MessageIcon from '@mui/icons-material/Message';
+import AttachEmailIcon from '@mui/icons-material/AttachEmail';
 
-const Footer = () => {
-    return (
-        <Box className="footer-container">
-            <Grid container spacing={3} sx={{ maxWidth: '1200px', mx: 'auto', px: 2 }}>
-                {/* Twende App Info */}
-                <Grid item xs={12} md={4}>
-                    <Typography variant="h6" gutterBottom>
-                        Twende App
-                    </Typography>
-                    <Typography variant="body2">
-                        Revolutionizing urban transportation. Book your bus rides easily and get to your destination on time with Twende!
-                    </Typography>
-                </Grid>
+function Footer() {
+  return (
+  <footer>
+    <div className='footer'>
+        <div className="column">
+          {/* <img src={FooterLogo} alt="" /> */}
+        </div>
+        <div className="column">
+          <h4>Resources</h4>
+          <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Get Started</li>
+          </ul>
+        </div>
+        <div className="column">
+          <h4>Find Us on Social media</h4>
+          <ul>
+            <li>X</li>
+            <li>Instagram</li>
+            <li>Youtube</li>
+            <li>Linkedin</li>
+          </ul>
+        </div>
+        <div className="column">
+          <h4>Contact Us</h4>
+          <ul>
+            <li>Call Us</li>
+            <li>Message Us</li>
+            <li>Email Us</li>
+          </ul>
+        </div>
+    </div>
+    <p>&copy; 2024 Tuende.com</p>
+  </footer>
+  )
+}
 
-                {/* Links */}
-                <Grid item xs={12} md={4}>
-                    <Typography variant="h6" gutterBottom>
-                        Quick Links
-                    </Typography>
-                    <Link href="/" underline="none">
-                        Home
-                    </Link>
-                    <Link href="/bookings" underline="none">
-                        Bookings
-                    </Link>
-                    <Link href="/contact" underline="none">
-                        Contact Us
-                    </Link>
-                    <Link href="/profile" underline="none">
-                        Your Profile
-                    </Link>
-                </Grid>
-
-                {/* Social Media */}
-                <Grid item xs={12} md={4}>
-                    <Typography variant="h6" gutterBottom>
-                        Connect with Us
-                    </Typography>
-                    <Box>
-                        <IconButton href="https://facebook.com" target="_blank">
-                            <Facebook />
-                        </IconButton>
-                        <IconButton href="https://twitter.com" target="_blank">
-                            <Twitter />
-                        </IconButton>
-                        <IconButton href="https://instagram.com" target="_blank">
-                            <Instagram />
-                        </IconButton>
-                        <IconButton href="https://linkedin.com" target="_blank">
-                            <LinkedIn />
-                        </IconButton>
-                    </Box>
-                </Grid>
-            </Grid>
-
-            <Box className="footer-bottom">
-                <Typography variant="body2">
-                    &copy; {new Date().getFullYear()} Twende App. All rights reserved.
-                </Typography>
-            </Box>
-        </Box>
-    );
-};
-
-export default Footer;
+export default Footer
